11961abb228f62b51cd9e4b8938ab25f87139721
Sascha Andres <sascha.andres@outlook.com>
2021-01-31 05:22:55 +0100

    feat: use path for log output

--- scanner/run.go
+++ scanner/run.go
@@ -10,13 +10,13 @@ import (
 func (scanner *Scanner) Run() error {
 	logger := scanner.logger.WithField("method", "Run")
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		logger.Infof("working on %s", info.Name())
+		logger.Infof("working on %s", path)
 		if err != nil {
 			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 			return err
 		}
 		if !info.IsDir() {
-			logger.Infof("starting hash for %s", info.Name())
+			logger.Infof("starting hash for %s", path)
 			_ = scanner.hashFile(info, path)
 		}
 		return nil
