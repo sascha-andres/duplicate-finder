38a0280f59c9c850a378fb2b4dbe030801f44027
Sascha Andres <sascha.andres@outlook.com>
2021-02-01 09:07:20 +0100

    feat: add number of files in directory for grouped reporter

--- reporter/grouped_console_reporter_finish.go
+++ reporter/grouped_console_reporter_finish.go
@@ -1,6 +1,9 @@
 package reporter
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 func (reporter *GroupedConsoleReporter) Finish() error {
 	fmt.Println("== potentially duplicated files ==")
@@ -19,7 +22,8 @@ func (reporter *GroupedConsoleReporter) Finish() error {
 		fmt.Println()
 		fmt.Println(fmt.Sprintf("Directory %s has files in common with:", outerDir))
 		for innerDirectory, countOfFiles := range commonFilesDirectory {
-			fmt.Println(fmt.Sprintf("  - %s with %d file(s)", innerDirectory, countOfFiles))
+			fileInfos, _ := ioutil.ReadDir(innerDirectory)
+			fmt.Println(fmt.Sprintf("  - %s with %d file(s) with %d file(s) in the directory", innerDirectory, countOfFiles, len(fileInfos)))
 		}
 	}
 	return nil
